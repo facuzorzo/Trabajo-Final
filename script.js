c822ccd8c85b4c1d2793430e42948466fe55cbdc
facuzorzo <128389765+facuzorzo@users.noreply.github.com>
2023-04-12 23:18:41 -0300

    Add files via upload
    arregladas imagenes

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const contenedor = document.getElementById("contenedor")
 fetch("./database.json")
 .then(Response => Response.json())
 
 .then(data =>{
-    for (const producto of productos) {
-        contac.innerHTML += `
-        <div>
-        <h2>${producto.name}</h2>
-        <h2>${producto.modelos}</h2>
-        <p>${producto.price}</p>
-        `
+    for (const producto of data) {
+        contenedor.innerHTML += `
+    <div class="phone">
+        <div class="phoneConteiner">
+        <img class="imgPhone" src="${producto.imagen}"/>
+        </div>
+        <h2>$${producto.price}</h2>
+        <h3>Hasta 12 cuotas sin interes</h3>
+        <p>${producto.name}${producto.modelo}</p>
+        <button class="btn-comprar" id="btn-comprar">Añadir al carrito<i class="bi bi-cart4"></i></button>
+
+       
+    </div>        `
     }
 })
+
+
 const carritoCargar = document.getElementById("btn-comprar")
 carritoCargar.addEventListener("click", () =>{
     Swal.fire('Añadiste este producto al carrito de compras')
